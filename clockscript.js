43e08557ac55d4d49365fccd9476c2656298a4ac
VladislavYamschikov <62208786+VladislavYamschikov@users.noreply.github.com>
2020-03-28 19:52:32 +0300

    1st clock

---
+++ clockscript.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    window.setInterval(
+     function(){
+         var d = new Date();
+         document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+     }
+   , 1000);
+ }
